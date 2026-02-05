bb5709f4620600a44f283da1df91af5a288720bd
PrasiddhShah <prasiddhshah.18@gmail.com>
2026-02-04 16:11:30 -0800

    models changed

---
+++ Backend/app/models/apollo_model.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel
+
+class CompanyResponse(BaseModel):
+    id: str
+    name: str
+    website_url: str | None = None
+    linkedin_url: str | None = None
+    twitter_url: str | None = None
+    angellist_url: str | None = None
+    logo_url: str | None = None
+
+class PersonResponseRaw(BaseModel):
+    id: str
+    first_name: str
+    last_name: str
+    title: str | None = None
+    has_email: bool = False
+    has_state: bool = False
+    has_country: bool = False
+
+class PersonResponseEnriched(BaseModel):
+    id: str
+    first_name: str
+    last_name: str
+    email: str | None = None
+    title: str | None = None
+    linkedin_url: str | None = None
+    photo_url: str | None = None
+    email_status: str | None = None
+    organization_id: str | None = None
+    headline: str | None = None
+    city: str | None = None
+    state: str | None = None
+    country: str | None = None
+
+class CompaniesAPIResponse(BaseModel):
+    organizations: list[CompanyResponse] = []
+
+class PeopleAPIResponse(BaseModel):
+    total_entries: int = 0
+    people: list[PersonResponseRaw] = []  
+
+class EnrichAPIResponse(BaseModel):
+    person: PersonResponseEnriched
